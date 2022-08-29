108096de32fe9b28ad4e796ae0e4337407bb0844
REDMOND\xiaoxiye <xiaoxiye@microsoft.com>
2022-08-29 14:54:08 +0800

    initial project

---
+++ Microsoft.Streamye.DesignPattern/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Microsoft.Streamye.DesignPattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
